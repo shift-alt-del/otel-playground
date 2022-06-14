1a0bde515b4ea108cb74faa981456125a3ef739c
w-ding <wding@confluent.io>
2022-06-14 15:48:50 +0900

    Add mysql connection pool.

--- online.py
+++ online.py
@@ -4,44 +4,39 @@ import json
 import time
 
 import flask
-import mysql
-import requests
-from kafka import KafkaProducer
+from mysql.connector.pooling import MySQLConnectionPool
 
+import mysql
+from jinja2 import Environment, FileSystemLoader
+from kafka import KafkaProducer
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.mysql import MySQLInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.instrumentation.kafka import KafkaInstrumentor
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
-    ConsoleSpanExporter,
 )
-
-# Initialize provider, jaeger exporter.
 
 from kafka_utils import create_topic_if_not_exist
 
-provider = TracerProvider(resource=Resource.create({SERVICE_NAME: 'api'}))
+# Initialize provider, jaeger exporter.
+provider = TracerProvider(
+    resource=Resource.create({SERVICE_NAME: 'api'}))
 provider.add_span_processor(
-    BatchSpanProcessor(JaegerExporter(agent_host_name='jaeger', agent_port=6831)))
+    BatchSpanProcessor(
+        JaegerExporter(
+            agent_host_name='jaeger', agent_port=6831)))
 trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
 
 # Initialize flask app.
 app = flask.Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
 KafkaInstrumentor().instrument()
 MySQLInstrumentor().instrument()
-
-# Get tracer.
-tracer = trace.get_tracer(__name__)
-
-# print('wait a few seconds for kafka to startup.')
-# time.sleep(5)
 
 # Initialize kafka topic
 conf = dict(bootstrap_servers=os.environ.get('DEMO_BOOTSTRAP_SERVER', 'kafka:9092'))
@@ -49,15 +44,23 @@ topic_name = 'async-queue'
 create_topic_if_not_exist(conf, topic_name)
 producer = KafkaProducer(**conf)
 
+cnxpool = MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=10,
+    user='example-user',
+    password='example-pw',
+    host='localhost',
+    database='example-db')
+
 
 @app.route('/')
 def home():
-    # todo: how to compose response?
     values = []
-    cnx = mysql.connector.connect(user='example-user', password='example-pw', host='localhost', database='example-db')
+
+    cnx = cnxpool.get_connection()
     cursor = cnx.cursor()
     cursor.execute("select st, ed, count from PV_COUNT order by st;")
-    for (st, ed, count) in cursor:
+    for (st, ed, count) in cursor.fetchall():
         values.append({
             'st': datetime.datetime.fromtimestamp(st // 1000),
             'ed': datetime.datetime.fromtimestamp(ed // 1000),
@@ -67,8 +70,6 @@ def home():
     cnx.close()
 
     producer.send(topic_name, json.dumps({'hello': 'world'}).encode('utf-8'))
-
-    from jinja2 import Environment, FileSystemLoader
 
     file_loader = FileSystemLoader('.')
     env = Environment(loader=file_loader)
